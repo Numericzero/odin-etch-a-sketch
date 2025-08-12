36fa428a79cbd813ce41d4c118c4848097559f9f
Numericzero <103614094+Numericzero@users.noreply.github.com>
2025-08-12 19:35:38 -0400

    Create a button to change size of the gridbox
    Created a button to prompt user to enter a new size for the
    gridbox between 1 and 100. Gridbox size is then displayed and a
    new grid is made.

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
+const DEFAULT_GRID_SIZE = 16;
+
 let container = document.querySelector(".container");
-for(let i=0; i<16; i++)
+
+function drawGrid(size)
 {
-    for(let j=0; j<16; j++)
+    while(container.firstChild != null)
+        container.firstChild.remove();
+    
+    for(let i=0; i<size; i++)
     {
-        let gridbox = document.createElement("div");
-        gridbox.classList.add("gridbox");
-        gridbox.addEventListener("mouseover", (e) => {
-            e.target.style.background = "gray";
-        });
-        container.appendChild(gridbox);
+        for(let j=0; j<size; j++)
+        {
+            let gridbox = document.createElement("div");
+
+            gridbox.classList.add("gridbox");
+            gridbox.style.width = 960/size + "px";
+            gridbox.style.height = 960/size + "px";
+            
+            gridbox.addEventListener("mouseover", (e) => {
+                e.target.style.background = "gray";
+            });
+            
+            container.appendChild(gridbox);
+        }
     }
 }
+
+let btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    let oldGridSize = document.querySelector("#grid-size");
+
+    let newGridSize = Number.parseInt(prompt("Enter the new grid size: ", oldGridSize.textContent));
+    if(Number.isNaN(newGridSize))
+        newGridSize = Number.parseInt(oldGridSize.textContent);
+    else if(newGridSize < 1)
+        newGridSize = 1;
+    else if(newGridSize > 100)
+        newGridSize = 100
+
+    drawGrid(newGridSize);
+
+    oldGridSize.textContent = newGridSize;
+})
+
+drawGrid(DEFAULT_GRID_SIZE);
